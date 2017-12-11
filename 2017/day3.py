dd2678ef1b533a761a233b974c22f4ca4a8b0849
Bart de Goede <b.de.goede@catawiki.nl>
2017-12-11 17:05:07 +0100

    PEP8

--- 2017/day3.py
+++ 2017/day3.py
@@ -22,9 +22,10 @@ from itertools import count
 def sum_spiral():
     a, i, j = {(0,0) : 1}, 0, 0
     for s in count(1, 2):
-        for (ds, di, dj) in [(0,1,0),(0,0,-1),(1,-1,0),(1,0,1)]:
-            for _ in xrange(s+ds):
-                i += di; j += dj
+        for (ds, di, dj) in [(0, 1, 0), (0, 0, -1), (1, -1, 0), (1, 0, 1)]:
+            for _ in xrange(s + ds):
+                i += di
+                j += dj
                 a[i,j] = sum(a.get((k,l), 0) for k in range(i-1,i+2)
                                              for l in range(j-1,j+2))
                 yield a[i,j]
