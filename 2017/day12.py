e5341826ef295834cab86e67dc3fa69259ff62af
Bart de Goede <b.de.goede@catawiki.nl>
2017-12-12 10:41:06 +0100

    Cleanup day12

--- 2017/day12.py
+++ 2017/day12.py
@@ -1,13 +1,4 @@
-class Node(object):
-    def __init__(self, name, connected_to):
-        self.name = name
-        self.connected_to = connected_to
-
-    def __repr__(self):
-        return 'Node <{}>: {}'.format(self.name, self.connected_to)
-
-
-def find_connected_nodes(graph, root=0):
+def find_connected_nodes(graph, root='0'):
     group = set([root])
     new = set([root])
     while new:
@@ -21,18 +12,11 @@ def find_connected_nodes(graph, root=0):
     return group
 
 if __name__ == '__main__':
-#     programs = """0 <-> 2
-# 1 <-> 1
-# 2 <-> 0, 3, 4
-# 3 <-> 2, 4
-# 4 <-> 2, 3, 6
-# 5 <-> 6
-# 6 <-> 4, 5"""
     nodes = {}
     with open('day12.txt', 'r') as f:
         for program in f:
             node, connected_to = program.split(' <-> ')
-            nodes[int(node)] = [int(n) for n in connected_to.split(', ')]
+            nodes[node] = connected_to.strip().split(', ')
 
     print 'Part 1:', len(find_connected_nodes(nodes))
 
