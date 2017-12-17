d9c01aa29dd831ec5414842bd8dcf2006c206c24
Bart de Goede <b.de.goede@catawiki.nl>
2017-12-17 11:18:58 +0100

    D172A - this is 10x faster, because we don't have to actually do the rotation

--- 2017/day17.py
+++ 2017/day17.py
@@ -15,20 +15,38 @@ def solve1():
 
 
 def solve2():
-    start = time.time()
     steps = 355
     spinlock_buffer = deque([0])
     for i in xrange(1, 50000001):
         spinlock_buffer.rotate(-steps)
         spinlock_buffer.append(i)
-        if i % 100000 == 0:
-            print 'Cycled {} times'.format(i)
 
     buff = list(spinlock_buffer)
-    print 'Took {} seconds'.format(time.time() - start)
+
     return buff[buff.index(0) + 1]
+
+
+def solve2a():
+    # we don't actually have to do the rotation of the array and insert
+    # values; we just need to keep track of the value we're inserting
+    # when we're inserting at position 1
+    steps = 355
+    position = 0
+    value = None
+    for i in xrange(1, 50000001):
+        new_position = ((position + steps) % i) + 1
+        if new_position == 1:
+            value = i
+        position = new_position
+
+    return value
 
 
 if __name__ == '__main__':
     print 'Part 1:', solve1()
+    start = time.time()
     print 'Part 2:', solve2()
+    print 'Took {} seconds'.format(time.time() - start)
+    start = time.time()
+    print 'Part 2a:', solve2a()
+    print 'Took {} seconds'.format(time.time() - start)
