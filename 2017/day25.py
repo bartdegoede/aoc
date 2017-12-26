8c9ce4e078407f0f253e814fc544dae15e33f668
Bart de Goede <b.de.goede@catawiki.nl>
2017-12-26 16:29:04 +0100

    Strings to mark state seems nicer

--- 2017/day25.py
+++ 2017/day25.py
@@ -2,24 +2,23 @@ if __name__ == '__main__':
     head = 0
     tape = {}
     steps = 12481997
-    a, b, c, d, e, f = range(6)
-    state = a
+    state = 'a'
     left = -1
     right = 1
 
     turing_machine = {
-        (a, 0): (1, right, b),
-        (a, 1): (0, left, c),
-        (b, 0): (1, left, a),
-        (b, 1): (1, right, d),
-        (c, 0): (0, left, b),
-        (c, 1): (0, left, e),
-        (d, 0): (1, right, a),
-        (d, 1): (0, right, b),
-        (e, 0): (1, left, f),
-        (e, 1): (1, left, c),
-        (f, 0): (1, right, d),
-        (f, 1): (1, right, a)
+        ('a', 0): (1, right, 'b'),
+        ('a', 1): (0, left, 'c'),
+        ('b', 0): (1, left, 'a'),
+        ('b', 1): (1, right, 'd'),
+        ('c', 0): (0, left, 'b'),
+        ('c', 1): (0, left, 'e'),
+        ('d', 0): (1, right, 'a'),
+        ('d', 1): (0, right, 'b'),
+        ('e', 0): (1, left, 'f'),
+        ('e', 1): (1, left, 'c'),
+        ('f', 0): (1, right, 'd'),
+        ('f', 1): (1, right, 'a')
     }
 
     for i, step in enumerate(range(steps)):
