bc0f407cc0bcc7a47ef7fc62b9da4d0359a1b189
Bart de Goede <bart.degoede@instacart.com>
2021-12-09 11:45:09 -0800

    Generators, not list comprehensions

--- 2021/day7.py
+++ 2021/day7.py
@@ -11,12 +11,12 @@ if __name__ == '__main__':
 
     sorted_positions = sorted(data)
     positions_median = median(sorted_positions)
-    print(f'Part 1: {int(sum([abs(pos - positions_median) for pos in sorted_positions]))}')
+    print(f'Part 1: {int(sum(abs(pos - positions_median) for pos in sorted_positions))}')
 
     max_position = sorted_positions[-1]
     results = []
     for i in range(max_position):
-        total_fuel = sum([fuel(abs(position - i)) for position in sorted_positions])
+        total_fuel = sum(fuel(abs(position - i)) for position in sorted_positions)
         results.append(total_fuel)
 
     print(f'Part 2: {min(results)}')
